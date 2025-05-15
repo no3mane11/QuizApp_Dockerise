dc958de910610824c02eed4ce4a4f491d5107ae9
sami3l <elhadraoui.sami@emsi-edu.ma>
2025-05-15 01:30:51 +0100

    herewegooo

--- quizapp-backend/src/main/java/com/quiz/quizapp/config/WebConfig.java
+++ quizapp-backend/src/main/java/com/quiz/quizapp/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("*") // ✅ autorise tous les domaines
+                        .allowedOrigins("https://quizapp-frontend-todeploy.vercel.app")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
